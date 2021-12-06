e98b01c637737f2ed974f570625a6d030d19ed68
Adam Eijdenberg <adam@continusec.com>
2021-12-06 18:19:41 +1100

    Part 2

---
+++ day/6/day6part2.py
@@ -0,0 +1,6 @@
+states = ([0] * 7) + ([0] * 9)
+for x in open('input').read().strip().split(','):
+    states[int(x)] += 1
+for i in range(256):
+    states = states[1:7] + [states[0] + states[7]] + states[8:18] + [states[0] + states[7]]
+print(sum(states))

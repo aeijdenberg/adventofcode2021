5f71e3c743e255effb9f8c26eb833250b4ae06d6
Adam Eijdenberg <adam@continusec.com>
2021-12-12 19:18:50 +1100

    Part 1 done

---
+++ 2021/12/day12.py
@@ -0,0 +1,25 @@
+edges = {}
+for line in open('input'):
+    a, b = line.strip().split('-')
+    for o, d in [(a, b), (b, a)]:
+        s = edges.get(o, set())
+        s.add(d)
+        edges[o] = s
+
+journeys = [['start']]
+finishes = 0
+while len(journeys):
+    j = journeys.pop()
+    for p in edges[j[-1]]:
+        if p == 'end':
+            print(j)
+            finishes += 1
+        elif p == 'start': # hmm? can you do this?
+            pass # let's assume not
+        elif p.upper() == p: # we can happily transit
+            journeys.append(j + [p])
+        else: # lower
+            if p not in j:
+                journeys.append(j + [p])
+
+print(finishes)

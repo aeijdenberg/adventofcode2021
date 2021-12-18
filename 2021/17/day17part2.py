defd8b2447f43c0f95dbc17dae8c93c8ae247638
Adam Eijdenberg <adam@continusec.com>
2021-12-18 14:14:49 +1100

    Part 2

---
+++ 2021/17/day17part2.py
@@ -0,0 +1,41 @@
+x_bit, y_bit = open('input').read().strip().split(' ', 2)[2].split(', ', 1)
+x1, x2, y1, y2 = [int(x) for x in x_bit[2:].split('..')] + [int(y) for y in y_bit[2:].split('..')]
+
+def doit(dx, dy):
+    x, y = 0, 0
+
+    max_y = y
+    while True:
+        x += dx
+        y += dy
+
+        max_y = max(max_y, y)
+
+        if dx > 0:
+            dx -= 1
+        elif dx < 0:
+            dx += 1
+
+        dy -= 1
+
+        if x1 <= x <= x2 and y1 <= y <= y2:
+            return True, max_y
+
+        if dx < 0 and x < x1:
+            return False, None
+
+        if dx > 0 and x > x2:
+            return False, None
+
+        if dy < 0 and y < y1:
+            return False, None
+
+zz = -9999999
+cc = 0
+for dx in range(1, x2 + 1):
+    for dy in range(-300, 300): # pretty dodgy
+        got_it, best = doit(dx, dy)
+        if got_it:
+            print(dx, dy)
+            cc += 1
+print(cc)

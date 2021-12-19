1e9f291a230b8456b090512f0b0f58e7345b0487
Adam Eijdenberg <adam@continusec.com>
2021-12-19 19:10:43 +1100

    Day 19 part 2

---
+++ 2021/19/day19part2.py
@@ -0,0 +1,67 @@
+import itertools
+
+class Scanner(object):
+    def __init__(self, id, b=None, s=None):
+        self.id = id
+        self.beacons = b if b else set()
+        self.scanner = s if s else (0, 0, 0)
+    
+    def __repr__(self):
+        return repr(self.beacons)
+    
+    def each_combo(self):
+        for pos_neg in itertools.product(*([[1,-1]]*3)):
+            for orders in itertools.permutations(range(3)):
+                yield Scanner(self.id, set(
+                    tuple(pos_neg[i] * xyz[orders[i]] for i in range(3)) for xyz in self.beacons
+                ), tuple(pos_neg[i] * self.scanner[orders[i]] for i in range(3)))
+
+    def overlaps(self, other, target):
+        for our_xyz in self.beacons:
+            for their_xyz in other.beacons:
+                offsets = list(their_xyz[i] - our_xyz[i] for i in range(3))
+                offset_scanner = Scanner(other.id, set(
+                    tuple(b[i] - offsets[i] for i in range(3)) for b in other.beacons
+                ), tuple(other.scanner[i] - offsets[i] for i in range(3)))
+                if len(self.beacons & offset_scanner.beacons) >= target:
+                    print("GOT ONE!!!")
+                    return offset_scanner
+        return None
+
+
+scanners = []
+for line in open('input'):
+    if line.startswith('---'):
+        scanners.append(Scanner(len(scanners)))
+    elif line.strip():
+        scanners[-1].beacons.add(tuple([int(x) for x in line.strip().split(',')]))
+
+known_bads = set()
+
+done_scanners, scanners = [scanners[0]], scanners[1:]
+while len(scanners):
+    done = False
+    for idx, scanner in enumerate(scanners):
+        for good_scanner in done_scanners:
+            key = tuple(sorted([good_scanner.id, scanner.id]))
+            if key in known_bads:
+                continue
+            print(key)
+            for version in scanner.each_combo():
+                adjusted_scanner = good_scanner.overlaps(version, 12)
+                if adjusted_scanner is not None:
+                    done_scanners = [adjusted_scanner] + done_scanners
+                    scanners = scanners[:idx] + scanners[idx+1:]
+                    done = True
+                    break
+            if done:
+                break
+            else:
+                known_bads.add(key)
+        if done:
+            break
+    if not done:
+        print("Giving up, no change.")
+        break
+
+print(max(sum(abs(a.scanner[i] - b.scanner[i]) for i in range(3)) for a, b in itertools.combinations(done_scanners, 2)))

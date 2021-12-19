7d9e4e2ae955311ab84f7033c3b2a7410d61040f
Adam Eijdenberg <adam@continusec.com>
2021-12-19 18:53:48 +1100

    Day 18, part 1

--- 2021/19/day19.py
+++ 2021/19/day19.py
@@ -1,14 +1,15 @@
 import itertools
 
 class Scanner(object):
-    def __init__(self, b=None):
+    def __init__(self, id, b=None):
+        self.id = id
         self.beacons = b if b else set()
     def __repr__(self):
         return repr(self.beacons)
     def each_combo(self):
         for pos_neg in itertools.product(*([[1,-1]]*3)):
             for orders in itertools.permutations(range(3)):
-                yield Scanner(set(
+                yield Scanner(self.id, set(
                     tuple(pos_neg[i] * xyz[orders[i]] for i in range(3)) for xyz in self.beacons
                 ))
 
@@ -16,7 +17,7 @@ class Scanner(object):
         for our_xyz in self.beacons:
             for their_xyz in other.beacons:
                 offsets = list(their_xyz[i] - our_xyz[i] for i in range(3))
-                offset_scanner = Scanner(set(
+                offset_scanner = Scanner(other.id, set(
                     tuple(b[i] - offsets[i] for i in range(3)) for b in other.beacons
                 ))
                 if len(self.beacons & offset_scanner.beacons) >= target:
@@ -28,25 +29,32 @@ class Scanner(object):
 scanners = []
 for line in open('input'):
     if line.startswith('---'):
-        scanners.append(Scanner())
+        scanners.append(Scanner(len(scanners)))
     elif line.strip():
         scanners[-1].beacons.add(tuple([int(x) for x in line.strip().split(',')]))
 
+known_bads = set()
 
 done_scanners, scanners = [scanners[0]], scanners[1:]
 while len(scanners):
     done = False
     for idx, scanner in enumerate(scanners):
-        for version in scanner.each_combo():
-            for good_scanner in done_scanners:
+        for good_scanner in done_scanners:
+            key = tuple(sorted([good_scanner.id, scanner.id]))
+            if key in known_bads:
+                continue
+            print(key)
+            for version in scanner.each_combo():
                 adjusted_scanner = good_scanner.overlaps(version, 12)
                 if adjusted_scanner is not None:
-                    done_scanners.append(adjusted_scanner)
+                    done_scanners = [adjusted_scanner] + done_scanners
                     scanners = scanners[:idx] + scanners[idx+1:]
                     done = True
                     break
             if done:
                 break
+            else:
+                known_bads.add(key)
         if done:
             break
     if not done:
